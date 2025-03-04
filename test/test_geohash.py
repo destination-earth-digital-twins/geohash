913e8bc8a6c6a94972bd38bd70b1baca36646273
Johannes Langvatn <johannestl@met.no>
2025-03-04 14:17:44 +0100

    Clean and rename

--- test/test_geohash.py
+++ test/test_geohash.py
@@ -5,19 +5,23 @@ sys.path.insert(0, '../geohash')
 
 import geohash
 
-def test_geohash():
 
-    #geohash for 42.6, -5.6:
+def test_encode():
     outhash = geohash.encode(42.6, -5.6)
     assert outhash == "ezs42e44yx96"
 
     outhash = geohash.encode(42.6, -5.6, precision=5)
     assert outhash == "ezs42"
+    outhash = geohash.encode(42.6, -5.6, precision=1)
 
+
+def test_decode():
     lat, lon =  geohash.decode('ezs42')
     assert lat == "42.6"
     assert lon == "-5.6"
 
+
+def test_decode_exactly():
     lat, lon, lat_err, lon_err = geohash.decode_exactly("ezs42")
     assert lat == 42.60498046875
     assert lon == -5.60302734375
